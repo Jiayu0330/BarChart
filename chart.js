7567ce248519590e534a85a58260f3f926ec2a21
Jiayu Jiang <you@example.com>
2019-03-05 14:26:00 -0500

    2

--- chart.js
+++ chart.js
@@ -1,9 +1,10 @@
-var dataP = d3.json("colors.json");
+var dataP = d3.json("colors.csv");
 
 var drawChart = function(colorData)
 {
   var width = 400;
   var height = 200;
+  var barWidth = width/colorData.length;
 
   var svg = d3.select("svg")
               .attr("width",width)
@@ -21,6 +22,23 @@ var drawChart = function(colorData)
      {
        return height = d.num*10;
      })
-
-
+     .attr("width",barWidth)
+     .attr("height",function(d)
+     {
+       return d.num*10;
+     })
+     .fill("color"，function(d)
+     {
+       return d.color;
+     })
 }
+
+dataP.then(function(data)
+{
+  console.log("data",data)
+  drawChart(dara);
+},
+function(err)
+{
+  console.log(err)
+});
